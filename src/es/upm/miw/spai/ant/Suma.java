22b46fadee47dbea3c6a10ef99bc7079704e557e
miw-upm <j.bernal@upm.es>
2013-09-20 12:53:00 +0200

    D20H12:52

---
+++ src/es/upm/miw/spai/ant/Suma.java
@@ -0,0 +1,15 @@
+package es.upm.miw.spai.ant;
+
+public class Suma {
+    public double getSuma(double operando1, double operando2) {
+        return operando1 + operando2;
+    }
+
+    public double incrementa(double operando) {
+        return operando + 1;
+    }
+    
+    public static void main(String[] args) {
+		System.out.println("Class Suma.......");
+	}
+}
